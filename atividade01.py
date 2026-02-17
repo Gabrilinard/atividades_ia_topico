a95729b2a6c0d6f62ab209a14eb5a9b3c80bf940
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2026-02-17 10:10:22 -0300

    Calcular consultas, chaves e valores a partir da entrada

--- atividade01.py
+++ atividade01.py
@@ -9,4 +9,12 @@ dimensao_modelo = 10
 tamanho_sequencia = 10
 dados_entrada = np.random.randn(1, tamanho_sequencia, dimensao_modelo)
 
-print(funcao_softmax(dados_entrada, eixo=-1).shape)
+pesos_q = np.random.randn(dimensao_modelo, dimensao_modelo)
+pesos_k = np.random.randn(dimensao_modelo, dimensao_modelo)
+pesos_v = np.random.randn(dimensao_modelo, dimensao_modelo)
+
+query = np.matmul(dados_entrada, pesos_q)
+chaves = np.matmul(dados_entrada, pesos_k)
+valores = np.matmul(dados_entrada, pesos_v)
+
+print(query.shape, chaves.shape, valores.shape)
