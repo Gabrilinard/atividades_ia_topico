ff476057f36a721b7fa1ec2aedaa8b729a48cf05
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2026-02-17 09:43:41 -0300

    Adicionar função softmax em NumPy

--- atividade01.py
+++ atividade01.py
@@ -1,7 +1,12 @@
 import numpy as np
 
+def funcao_softmax(valores, eixo=-1):
+    valores = valores - np.max(valores, axis=eixo, keepdims=True)
+    expoentes = np.exp(valores)
+    return expoentes / np.sum(expoentes, axis=eixo, keepdims=True)
+
 dimensao_modelo = 10
 tamanho_sequencia = 10
 dados_entrada = np.random.randn(1, tamanho_sequencia, dimensao_modelo)
 
-print(dados_entrada.shape)
+print(funcao_softmax(dados_entrada, eixo=-1).shape)
