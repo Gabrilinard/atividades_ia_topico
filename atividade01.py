50f636efaf05b12fa53b507648020bbb205072a1
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2026-02-17 11:33:44 -0300

    Encapsular mecanismo de atenção em classe MecanismoAtencao

--- atividade01.py
+++ atividade01.py
@@ -7,29 +7,31 @@ def funcao_softmax(valores, eixo=-1):
     expoentes = np.exp(valores)
     return expoentes / np.sum(expoentes, axis=eixo, keepdims=True)
 
-def atencao_produto_escalar_escalonado(entradas, pesos_consulta, pesos_chave, pesos_valor):
-    consultas = np.matmul(entradas, pesos_consulta)
-    chaves = np.matmul(entradas, pesos_chave)
-    valores = np.matmul(entradas, pesos_valor)
+class MecanismoAtencao:
+    def __init__(self, dimensao_modelo):
+        self.dimensao_modelo = dimensao_modelo
+        self.pesos_consulta = np.random.randn(dimensao_modelo, dimensao_modelo)
+        self.pesos_chave = np.random.randn(dimensao_modelo, dimensao_modelo)
+        self.pesos_valor = np.random.randn(dimensao_modelo, dimensao_modelo)
 
-    dimensao_chaves = consultas.shape[-1]
-    scores = np.matmul(consultas, np.swapaxes(chaves, -2, -1))
-    scores = scores / np.sqrt(dimensao_chaves)
-    pesos_atencao = funcao_softmax(scores, eixo=-1)
-    saida = np.matmul(pesos_atencao, valores)
-    return saida, pesos_atencao
+    def calcular_atencao(self, entradas):
+        consultas = np.matmul(entradas, self.pesos_consulta)
+        chaves = np.matmul(entradas, self.pesos_chave)
+        valores = np.matmul(entradas, self.pesos_valor)
+
+        dimensao_chaves = consultas.shape[-1]
+        scores = np.matmul(consultas, np.swapaxes(chaves, -2, -1))
+        scores = scores / np.sqrt(dimensao_chaves)
+        pesos_atencao = funcao_softmax(scores, eixo=-1)
+        saida = np.matmul(pesos_atencao, valores)
+        return saida, pesos_atencao
 
 dimensao_modelo = 10
 tamanho_sequencia = 10
 dados_entrada = np.random.randn(1, tamanho_sequencia, dimensao_modelo)
 
-pesos_consulta = np.random.randn(dimensao_modelo, dimensao_modelo)
-pesos_chave = np.random.randn(dimensao_modelo, dimensao_modelo)
-pesos_valor = np.random.randn(dimensao_modelo, dimensao_modelo)
-
-representacao_vetorial, pesos_atencao = atencao_produto_escalar_escalonado(
-    dados_entrada, pesos_consulta, pesos_chave, pesos_valor
-)
+mecanismo_atencao = MecanismoAtencao(dimensao_modelo)
+representacao_vetorial, pesos_atencao = mecanismo_atencao.calcular_atencao(dados_entrada)
 
 plt.figure(figsize=(10, 8))
 sns.heatmap(pesos_atencao[0], annot=True, cmap='viridis')
